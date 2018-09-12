fa363103f3f8eafc4b06776dbe274195a9866387
Philip Wellener <42978547+philipwellener@users.noreply.github.com>
2018-09-12 14:07:18 -0400

    Add files via upload

---
+++ src/SmileClass.java
@@ -0,0 +1,23 @@
+
+/**
+ * 
+ * @author Philip Wellener
+ * Period 7
+ * Smiley Face
+ *
+ */
+public class SmileClass 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.println("\\\\\\\\\\\\");
+	System.out.println("| * * |");
+	System.out.println("|  *  |");
+	System.out.println("| \\_/ |");
+	System.out.println("\\____/");
+	
+
+	}
+
+}
